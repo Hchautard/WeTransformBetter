16beec7bcb3d951ca3af501eebc11127b3300da2
hugolh <75566783+hugolh@users.noreply.github.com>
2024-09-16 10:57:02 +0200

    feat: inverse image

--- src/image.js
+++ src/image.js
@@ -23,7 +23,7 @@ function ImagePage() {
         setImage(null);
     };
 
-    const handleImageOperation = () => {
+    const rotateImage = () => {
         const rotatedImage = document.createElement('canvas');
         const ctx = rotatedImage.getContext('2d');
         const img = new Image();
@@ -42,6 +42,34 @@ function ImagePage() {
         };
     };
 
+    const inverseImage = () => {
+        const canvas = document.createElement('canvas');
+        const ctx = canvas.getContext('2d');
+        const img = new Image();
+        img.src = image;
+
+        img.onload = () => {
+            canvas.width = img.width;
+            canvas.height = img.height;
+
+            ctx.drawImage(img, 0, 0);
+
+            const imageData = ctx.getImageData(0, 0, canvas.width, canvas.height);
+            const data = imageData.data;
+
+            for (let i = 0; i < data.length; i += 4) {
+                data[i] = 255 - data[i];
+                data[i + 1] = 255 - data[i + 1];
+                data[i + 2] = 255 - data[i + 2];
+            }
+
+            ctx.putImageData(imageData, 0, 0);
+
+            const invertedImageUrl = canvas.toDataURL();
+            setImage(invertedImageUrl);
+        };
+    };
+
     return (
     <>
     <ResponsiveAppBar/>
@@ -51,7 +79,8 @@ function ImagePage() {
             {image && <img src={image} alt="Uploaded Image" />}
             <div>
                 {image && <Button variant="contained" onClick={deleteImage}>Delete image</Button>}
-                {image && <Button variant="contained" onClick={handleImageOperation}>Tourne</Button>}
+                {image && <Button variant="contained" onClick={rotateImage}>Tourne</Button>}
+                {image && <Button variant="contained" onClick={inverseImage}>Inverse</Button>}
             </div>
            
         </Container>
