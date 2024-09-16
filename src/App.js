e8e540f8585c44448e14d94334b37e61f39260ba
hugolh <75566783+hugolh@users.noreply.github.com>
2024-09-16 10:18:29 +0200

    load image

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import './App.css';
 import Home from './home';
 import { BrowserRouter as Router, Route, Routes} from 'react-router-dom';
+import ImagePage from './image';
 
 function App() {
   return (
@@ -8,6 +9,7 @@ function App() {
      <Router>
       <Routes>
         <Route path="/" element={<Home />} />
+        <Route path="/image" element={<ImagePage />} />
       </Routes>
       </Router>
    </body>
